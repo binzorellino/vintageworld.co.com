88bbdb90ead5fed7e32b17d1b10295056a246826
binzorellino <boldisimre@gmail.com>
2020-01-12 11:58:17 +0100

    ADD instagram section

---
+++ template-parts/sections/section-instagram.php
@@ -0,0 +1,18 @@
+<?php
+$post_994 = get_post(icl_object_id( 994, 'editable-elements', false ));
+if ( $post_994 ) :
+?>
+<section class="instagram-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="instagram__title"><?php echo get_the_title( $post_994->ID ); ?></h2>
+        <?php echo do_shortcode('[instagram-feed]'); ?>
+      </div>
+    </div>
+  </div>
+</section>
+<?php 
+endif;
+wp_reset_postdata();
+?>
