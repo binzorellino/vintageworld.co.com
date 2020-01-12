5b93ab766aebf9761059ade928c8a020d3657c03
binzorellino <boldisimre@gmail.com>
2020-01-12 11:42:21 +0100

    ADD site footer

--- footer.php
+++ footer.php
@@ -1,5 +1,21 @@
-    <?php wp_footer(); ?>
+<footer class="site-footer">
+  <div class="site-footer__top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p class="site-footer__legal-notice">*All prices incl. legal value added tax plus shipping costs and cash on delivery charges, if not otherwise described</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="site-footer__bottom"></div>
+</footer>
 
-  </body>
+</div><!-- #site-canvas -->
+</main><!-- #main -->
+
+<?php wp_footer(); ?>
+
+</body>
 
 </html>
