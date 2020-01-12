fdc15aefabd5da2e7bcfa5477d06adad00d0e69f
binzorellino <boldisimre@gmail.com>
2020-01-12 12:12:07 +0100

    ADD default page template

--- page.php
+++ page.php
@@ -1,13 +1,21 @@
 <?php
-
-  get_header();
-
-  while ( have_posts() ) : the_post();
-    get_template_part( 'template-parts/content', get_post_type() );
-  endwhile; wp_reset_postdata();
-
-  get_sidebar();
-
-  get_footer();
-
+get_header();
+?>
+<div class="tamplate-page">
+  <div class="container text-container">
+    <div class="row">
+      <div class="col-md-12">
+        <?php
+        wp_breadcrumbs_generator();
+        while ( have_posts() ) : the_post();
+          get_template_part( 'template-parts/content', get_post_type() );
+        endwhile; wp_reset_postdata();
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+get_sidebar();
+get_footer();
 ?>
